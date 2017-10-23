1873d7f9e2e443cc1997e5f352f3f4cbcdad46bc
Gwada <dimitri.lc@orange.fr>
2017-10-23 18:36:31 +0200

    modif ft_itoa

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include "libft.h"
+int		FD = 0;
 void	try_ft_putchar_fd(char);
 void	try_ft_putstr_fd(char *);
 void	try_ft_putendl_fd(char *);
@@ -25,36 +26,28 @@ return (0);
 
 void	try_ft_putnbr_fd(int n)
 {
-	int fd;
-
-	fd = creat("test ft_putnbr_fd.txt", S_IRWXU);
-	ft_putnbr_fd(n, fd);
-	close (fd);
+	FD = creat("test ft_putnbr_fd.txt", S_IRWXU);
+	ft_putnbr_fd(n, FD);
+	close (FD);
 }
 
 void	try_ft_putchar_fd(char c)
 {
-	int fd;
-	
-	fd = creat("test ft_putchar_fd.txt", S_IRWXU);
-	ft_putchar_fd(c, fd);
-	close (fd);
+	FD = creat("test ft_putchar_fd.txt", S_IRWXU);
+	ft_putchar_fd(c, FD);
+	close (FD);
 }
 
 void	try_ft_putstr_fd(char *s)
 {
-	int fd;
-	
-	fd = creat("test ft_putstr_fd.txt", S_IRWXU);
-	ft_putstr_fd(s, fd);
-	close (fd);
+	FD = creat("test ft_putstr_fd.txt", S_IRWXU);
+	ft_putstr_fd(s, FD);
+	close (FD);
 }
 
 void	try_ft_putendl_fd(char *s)
 {
-	int fd;
-	
-	fd = creat("test ft_putnbr_fd.txt", S_IRWXU);
-	ft_putendl_fd(s, fd);
-	close(fd);
+	FD = creat("test ft_putnbr_fd.txt", S_IRWXU);
+	ft_putendl_fd(s, FD);
+	close(FD);
 }
