32817c7759cc9290c8676041322daccfe5c64607
Gwada <dimitri.lc@orange.fr>
2017-10-23 12:33:33 +0200

    ajout structure dans .h

--- libft.h
+++ libft.h
@@ -7,6 +7,16 @@
 #include <unistd.h>
 
 /*
+**	STRUCTURES
+*/
+typedef	struct		S_LIST
+{
+	void			*content;
+	size_t			content_size;
+	struct	s_list	*next;
+}					T_LIST;
+
+/*
 **	PROTOTYPES
 */
 void	ft_putchar(char);
