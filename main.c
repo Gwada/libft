dcf81e966abffa4e662ce67b2bc53c27e233dce5
Gwada <dimitri.lc@orange.fr>
2017-10-25 00:13:59 +0200

    ajout ft_strncat.c + maj

--- main.c
+++ main.c
@@ -28,8 +28,9 @@ int		main(int argc, char **argv)
 	ft_putstr(ft_memmove(SRC + 5, SRC2, 4));
 	ft_putstr(ft_memchr(SRC, ' ', 16));
 	ft_putnbr(ft_memcmp(SRC2, SRC + 1, 13));
-	ft_putstr(ft_strdup(SRC));*/
+	ft_putstr(ft_strdup(SRC));
 	ft_putstr(ft_strcat(SRC, SRC2));
+	ft_putstr(ft_strncat(SRC, SRC2, 15));*/
 	/*ft_putnbr(ft_strlen("123456"));
 	ft_putstr(ft_strcpy(DEST, SRC));
 	ft_putstr(ft_strncpy(DEST, SRC, 10));
