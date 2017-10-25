8e6b109338234cde2fe0a1e8a893d89739119534
Gwada <33009090+Gwada@users.noreply.github.com>
2017-10-25 18:00:47 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -7,7 +7,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 
 	i = ft_strlen(dst);
 	j = -1;
-	while (i + ++j < dstsize - 1)
+	while ( i  +  ++ j  <  dstsize - 1 && src [j])
 		dst[i + j] = src[j];
 	dst[i + j] = '\0';
 	return (i + ft_strlen(src));
