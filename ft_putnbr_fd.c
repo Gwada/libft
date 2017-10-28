ec5693df53350d631bd28d61130f1635c479eaa6
Gwada <dimitri.lc@orange.fr>
2017-10-29 01:34:01 +0200

    modif ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -7,7 +7,7 @@ void	ft_putnbr_fd(int n, int fd)
 	ui = 1;
 	if (n < 0)
 		ft_putchar_fd('-', fd);
-	ui = (n < 0) ? n * -1 : n;
+	ui = (n < 0) ? -n : n;
 	if (ui > 9)
 		ft_putnbr_fd(ui / 10, fd);
 	ft_putchar_fd(ui % 10 + '0', fd);
