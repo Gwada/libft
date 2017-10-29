40a998cd87cb6d41cdec8628fdb36f48a8a92ba5
Gwada <dimitri.lc@orange.fr>
2017-10-29 02:22:34 +0200

    maj ft_strsplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -2,16 +2,15 @@
 
 char	**ft_strsplit(char const *s, char c)
 {
-	int		i;
 	int		nbwords;
 	int		nbletter;
 	char	**words;
 
-	i = -1;
 	nbwords = ft_countwords(s, c, 0);
 	if (!(words = (char**)malloc(sizeof(char*) * nbwords + 1)))
 		return (NULL);
-	while (++i < nbwords && *s)
+	words[nbwords] = '\0';
+	while (*s)
 	{
 		while (*s == c)
 			s++;
@@ -20,9 +19,7 @@ char	**ft_strsplit(char const *s, char c)
 			nbletter++;
 		if (!*s)
 			nbletter++;
-		words[i] = (char*)malloc(sizeof(char) * nbletter + 1);
-		ft_strncpy(words[i], s - nbletter, nbletter);
+		*words++ = ft_strsub(s - nbletter, 0, nbletter);
 	}
-	words[nbwords] = '\0';
-	return (words);
+	return (words - nbwords);
 }
